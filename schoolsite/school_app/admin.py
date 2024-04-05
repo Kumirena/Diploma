b53fc2ea4151f2c30c7d10ad01bca76ae879beed
Irina <you@example.com>
2024-04-06 01:45:50 +0300

    first commit

---
+++ schoolsite/school_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from school_app.models import User, Course, Teacher, Student, Tutor, Lesson, Grade
+
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Tutor)
+admin.site.register(Lesson)
+admin.site.register(Grade)
+
